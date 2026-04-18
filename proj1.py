0afe3b3ca2b9f5e5a6b2767572f7e00cfc07eddc
Guadalupe Cervantes <gcerva11@calpoly.edu>
2026-04-18 12:43:21 -0700

    .

--- proj1.py
+++ proj1.py
@@ -1,1 +1,19 @@
 #complete your tasks in this file
+@dataclass 
+class GlobeRect(frozen=True):
+lo_lat: float
+hi_lat: float
+west_long: float
+east_long: float
+
+class Region:
+    rect: GlobeRect
+    name: str
+    terrain: "ocean" | "mountains" | "forest" | "other"
+
+class RegionCondition
+    region: Region
+    year: int
+    pop: int
+    ghg_rate: float
+
