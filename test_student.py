6bf2027114a460498fc561698f434f44f0bfb8de
Guadalupe Cervantes <gcerva11@calpoly.edu>
2026-04-20 17:48:06 -0700

    first half of tests

--- test_student.py
+++ test_student.py
@@ -4,25 +4,59 @@ from proj1 import *
 #these do not contribute positivly to your grade. 
 #but your grade will be lowered if they are missing
 
-class TestRegionFunctions(unittest.TestCase):
+class TestStudent(unittest.TestCase):
+    def test_emissions_per_capita_normal(self) -> None:
+        self.assertAlmostEqual(emissions_per_capita(San_Francisco), 5.0, places=4)
 
-    def setUp(self):
-        pass
+    def test_emissions_per_capita_zero_population(self) -> None:
+        self.assertAlmostEqual(emissions_per_capita(Mediterranean_Sea), 0.0, places=4)
 
-    def test_holder(self):
-        pass
-def test_emissions_per_capita1(self) -> None:
-    pass
-def test_area(self) -> None:
-    pass
-def test_emissions_per_square_km(self)->None:
-    pass
-def test_dataset(self)->None:
-    pass
+    def test_emissions_per_capita_fraction(self) -> None:
+        tiny = RegionCondition(
+            region=Region(
+                rect=GlobeRect(0.0, 1.0, 0.0, 1.0),
+                name="Tiny",
+                terrain="other"
+            ),
+            year=2025,
+            pop=2,
+            ghg_rate=1.0
+        )
+        self.assertAlmostEqual(emissions_per_capita(tiny), 0.5, places=4)
+#######################################################################################################
+    def test_area_positive(self) -> None:
+        self.assertTrue(area(San_Francisco.region.rect) > 0)
 
-def project_condition(rc, years)->None:
-    pass
+    def test_area_zero_width(self) -> None:
+        gr = GlobeRect(10.0, 20.0, 5.0, 5.0)
+        self.assertAlmostEqual(area(gr), 0.0, places=4)
 
+    def test_area_wraparound_dateline(self) -> None:
+        gr = GlobeRect(10.0, 20.0, 170.0, -170.0)
+        self.assertTrue(area(gr) > 0)
 
-if __name__ == '__main__':
+    def test_area_small_region(self) -> None:
+        gr = GlobeRect(37.7, 37.8, -122.5, -122.4)
+        self.assertTrue(area(gr) > 0)
+#######################################################################################################
+    def test_emissions_per_square_km_normal(self) -> None:
+        self.assertTrue(emissions_per_square_km(San_Francisco) > 0)
+
+    def test_emissions_per_square_km_zero_emissions(self) -> None:
+        self.assertAlmostEqual(emissions_per_square_km(Mediterranean_Sea), 0.0, places=4)
+
+    def test_emissions_per_square_km_zero_area(self) -> None:
+        rc = RegionCondition(
+            region=Region(
+                rect=GlobeRect(10.0, 10.0, 20.0, 30.0),
+                name="Flat Line",
+                terrain="other"
+            ),
+            year=2025,
+            pop=1000,
+            ghg_rate=5000.0
+        )
+        self.assertAlmostEqual(emissions_per_square_km(rc), 0.0, places=4)
+
+if __name__ == "__main__":
     unittest.main()
